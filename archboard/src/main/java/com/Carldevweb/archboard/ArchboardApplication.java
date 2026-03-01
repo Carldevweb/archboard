07e403699e6c8d40e6876058309d712a876a10a9
Carldevweb <carlflahault@gmail.com>
2026-03-01 19:05:33 +0100

    Initial commit - Spring Boot 4 + Java 21 base setup

---
+++ archboard/src/main/java/com/Carldevweb/archboard/ArchboardApplication.java
@@ -0,0 +1,13 @@
+package com.Carldevweb.archboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchboardApplication.class, args);
+	}
+
+}
